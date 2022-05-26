ad3dcb5f71329d39d1eab9ac82fb99ba2b39ccf6
minsssg <minseock5202@gmail.com>
2022-05-27 00:35:51 +0900

    hello world 사진 저장소 다시 만들어보기

---
+++ src/helloworld/model/User.java
@@ -0,0 +1,42 @@
+package helloworld.model;
+
+public class User {
+
+	private String id;
+	private String password;
+	private String name;
+	private String gender;
+	private String phoneNumber;
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+}
